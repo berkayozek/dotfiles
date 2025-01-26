b2bbbc6bc15f335289f27f21da6da6b409d428c1
Berkay Özek <berkayozek@hotmail.com>
2025-01-26 22:45:54 +0100

    Added go and java formatters and modified lsp code action mapping

--- .config/nvim/lua/plugin/conform.lua
+++ .config/nvim/lua/plugin/conform.lua
@@ -20,6 +20,8 @@ return {
 		-- Define your formatters
 		formatters_by_ft = {
 			lua = { "stylua" },
+            go = { "goimports" },
+            java = { "google-java-format" }
 		},
 		-- Set default options
 		default_format_opts = {
