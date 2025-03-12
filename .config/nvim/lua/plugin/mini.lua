3a6499822d970816be55e955c7d27e18e5bf3db2
Berkay Ozek <berkayozek@hotmail.com>
2025-03-12 15:39:59 +0100

    feat(nvim): add mini.ai plugin

--- .config/nvim/lua/plugin/mini.lua
+++ .config/nvim/lua/plugin/mini.lua
@@ -2,6 +2,7 @@ return {
 	"echasnovski/mini.nvim",
 	version = false,
 	config = function()
+		require("mini.ai").setup()
 		require("mini.comment").setup()
 		require("mini.move").setup()
 		require("mini.pairs").setup()
