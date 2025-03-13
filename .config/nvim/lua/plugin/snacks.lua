5176046e94c34f675df942b75060de8b82527e29
Berkay Ozek <berkayozek@hotmail.com>
2025-03-13 14:37:38 +0100

    feat(nvim): add indent plugin of snacks.nvim

--- .config/nvim/lua/plugin/snacks.lua
+++ .config/nvim/lua/plugin/snacks.lua
@@ -4,6 +4,13 @@ return {
 		words = {},
 		input = {},
 		lazygit = {},
+		indent = {
+			filter = function(buf)
+				local exclude = { "markdown", "text" }
+				local ft = vim.bo[buf].filetype
+				return not vim.tbl_contains(exclude, ft)
+			end,
+		},
 	},
 	keys = {
 		{
