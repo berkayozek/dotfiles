4678af73bcadffd29dc62b86587eebe3550c4350
Berkay Ozek <berkayo@amazon.com>
2025-03-13 21:45:53 +0100

    refactor(nvim): replace nvim-tree and zen-mode with snacks.nvim

--- .config/nvim/lua/plugin/snacks.lua
+++ .config/nvim/lua/plugin/snacks.lua
@@ -1,17 +1,25 @@
 return {
 	"folke/snacks.nvim",
+	lazy = true,
 	opts = {
 		words = {},
 		input = {},
 		lazygit = {},
 		indent = {
 			filter = function(buf)
-				local exclude = { "markdown", "text" }
+				local exclude = { "markdown", "text", "leetcode.nvim" }
 				local ft = vim.bo[buf].filetype
 				return not vim.tbl_contains(exclude, ft)
 			end,
 		},
 		zen = {},
+		picker = {
+			sources = {
+				explorer = {
+					auto_close = true,
+				},
+			},
+		},
 	},
 	keys = {
 		{
@@ -27,6 +35,13 @@ return {
 				Snacks.zen()
 			end,
 			desc = "Toggle zen mode",
+		},
+		{
+			"<Tab>",
+			function()
+				Snacks.explorer()
+			end,
+			desc = "Toggle file explorer",
 		},
 	},
 }
