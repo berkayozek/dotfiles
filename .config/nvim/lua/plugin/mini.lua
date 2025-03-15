608a0263e2e78f2701936d224ae5f1bdfb2a70a9
Berkay Özek <berkayozek@hotmail.com>
2025-03-15 22:24:04 +0100

    feat(nvim): add hipatterns from mini.nvim and remove todo-comments.nvim

--- .config/nvim/lua/plugin/mini.lua
+++ .config/nvim/lua/plugin/mini.lua
@@ -8,10 +8,23 @@ return {
 		require("mini.pairs").setup()
 		require("mini.starter").setup()
 
-		local gen_loader = require("mini.snippets").gen_loader
-		require("mini.snippets").setup({
+		local snippets = require("mini.snippets")
+		local gen_loader = snippets.gen_loader
+		snippets.setup({
 			snippets = {
 				gen_loader.from_lang(),
+			},
+		})
+
+		local hipatterns = require("mini.hipatterns")
+		hipatterns.setup({
+			highlighters = {
+				fixme = { pattern = "()%s?FIXME:?()", group = "MiniHipatternsFixme" },
+				hack = { pattern = "()%s?HACK:?()", group = "MiniHipatternsHack" },
+				todo = { pattern = "()%s?TODO:?()", group = "MiniHipatternsTodo" },
+				note = { pattern = "()%s?NOTE:?()", group = "MiniHipatternsNote" },
+
+				hex_color = hipatterns.gen_highlighter.hex_color(),
 			},
 		})
 	end,
