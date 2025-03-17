8d5d0f9cf014d9eac494c7e9c162ff94be3fc6d5
Berkay Özek <berkayozek@hotmail.com>
2025-03-17 23:45:06 +0100

    fix(nvim): resolve keymap issue for Java LSP

--- .config/nvim/lua/plugin/lsp.lua
+++ .config/nvim/lua/plugin/lsp.lua
@@ -75,25 +75,33 @@ return {
 				require("blink.cmp").get_lsp_capabilities() or {},
 				opts.capabilities or {}
 			)
-			local on_attach = function(_, bufnr)
-				local function buf_set_keymap(mode, lhs, rhs, desc)
-					vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc, noremap = true, silent = true })
-				end
+			vim.api.nvim_create_autocmd("LspAttach", {
+				desc = "LSP actions",
+				callback = function(event)
+					local function buf_set_keymap(mode, lhs, rhs, desc)
+						vim.keymap.set(
+							mode,
+							lhs,
+							rhs,
+							{ buffer = event.buf, desc = desc, noremap = true, silent = true }
+						)
+					end
 
-				buf_set_keymap("n", "K", vim.lsp.buf.hover, "Show hover information")
-				buf_set_keymap("n", "gd", vim.lsp.buf.definition, "Go to definition")
-				buf_set_keymap("n", "gD", vim.lsp.buf.declaration, "Go to declaration")
-				buf_set_keymap("n", "gi", vim.lsp.buf.implementation, "Go to implementation")
-				buf_set_keymap("n", "go", vim.lsp.buf.type_definition, "Go to type definition")
-				buf_set_keymap("n", "gr", vim.lsp.buf.references, "Show references")
-				buf_set_keymap("n", "gs", vim.lsp.buf.signature_help, "Show signature help")
-				buf_set_keymap("n", "<Leader>rn", vim.lsp.buf.rename, "Rename symbol")
-				buf_set_keymap({ "n", "x" }, "<Leader>ca", vim.lsp.buf.code_action, "Show code actions")
-				buf_set_keymap("n", "<leader>e", vim.diagnostic.open_float, "Open diagnostic float")
-				buf_set_keymap("n", "[d", vim.diagnostic.goto_prev, "Go to previous diagnostic")
-				buf_set_keymap("n", "]d", vim.diagnostic.goto_next, "Go to next diagnostic")
-				buf_set_keymap("n", "<leader>q", vim.diagnostic.setloclist, "Set location list")
-			end
+					buf_set_keymap("n", "K", vim.lsp.buf.hover, "Show hover information")
+					buf_set_keymap("n", "gd", vim.lsp.buf.definition, "Go to definition")
+					buf_set_keymap("n", "gD", vim.lsp.buf.declaration, "Go to declaration")
+					buf_set_keymap("n", "gi", vim.lsp.buf.implementation, "Go to implementation")
+					buf_set_keymap("n", "go", vim.lsp.buf.type_definition, "Go to type definition")
+					buf_set_keymap("n", "gr", vim.lsp.buf.references, "Show references")
+					buf_set_keymap("n", "gs", vim.lsp.buf.signature_help, "Show signature help")
+					buf_set_keymap("n", "<Leader>rn", vim.lsp.buf.rename, "Rename symbol")
+					buf_set_keymap({ "n", "x" }, "<Leader>ca", vim.lsp.buf.code_action, "Show code actions")
+					buf_set_keymap("n", "<leader>e", vim.diagnostic.open_float, "Open diagnostic float")
+					buf_set_keymap("n", "[d", vim.diagnostic.goto_prev, "Go to previous diagnostic")
+					buf_set_keymap("n", "]d", vim.diagnostic.goto_next, "Go to next diagnostic")
+					buf_set_keymap("n", "<leader>q", vim.diagnostic.setloclist, "Set location list")
+				end,
+			})
 
 			mason_lspconfig.setup({
 				handlers = {
@@ -106,7 +114,6 @@ return {
 
 						lspconfig[server_name].setup({
 							capabilities = capabilities,
-							on_attach = on_attach,
 							settings = settings,
 						})
 					end,
