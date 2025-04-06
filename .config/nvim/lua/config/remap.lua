369f5c53bc6d751baaf6ee5edc95457b216543b3
Berkay Özek <berkayozek@hotmail.com>
2025-04-07 00:23:41 +0200

    feat(nvim): add new keymaps

--- .config/nvim/lua/config/remap.lua
+++ .config/nvim/lua/config/remap.lua
@@ -13,3 +13,16 @@ map.set("n", "<leader>Y", '"+Y', { noremap = true, desc = "Copy to clipboard" })
 -- Paste from the system clipboard
 map.set("n", "<leader>p", '"+p', { noremap = true, desc = "Paste from clipboard" })
 map.set("v", "<leader>p", '"+p', { noremap = true, desc = "Paste from clipboard" }) -- For visual mode
+
+-- save file
+map.set({ "i", "x", "n", "s" }, "<leader>w", "<cmd>w<cr><esc>", { desc = "Save File" })
+
+-- quit
+map.set("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit All" })
+
+-- Preserve visual selection when shifting text left (<) or right (>)
+map.set("x", "<", "<gv", { noremap = true, silent = true, desc = "Shift left and keep visual selection" })
+map.set("x", ">", ">gv", { noremap = true, silent = true, desc = "Shift right and keep visual selection" })
+
+-- lazy
+map.set("n", "<leader>l", "<cmd>Lazy<cr>", { desc = "Lazy" })
