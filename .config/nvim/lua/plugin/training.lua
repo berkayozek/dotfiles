273896cc57a64b199ed9b88010c0379b3972ed81
Berkay Özek <berkayozek@hotmail.com>
2025-04-09 23:26:02 +0200

    feat(nvim): add keymaps for leetcode.nvim

--- .config/nvim/lua/plugin/training.lua
+++ .config/nvim/lua/plugin/training.lua
@@ -17,6 +17,14 @@ return {
 				},
 			},
 		},
+		keys = {
+			{ "<leader>ll", "<cmd>Leet list<cr>", desc = "Leet list questions" },
+			{ "<leader>lr", "<cmd>Leet run<cr>", desc = "Leet run question" },
+			{ "<leader>lt", "<cmd>Leet tabs<cr>", desc = "Leet tabs" },
+			{ "<leader>lc", "<cmd>Leet console<cr>", desc = "Leet toggle console" },
+			{ "<leader>ls", "<cmd>Leet submit<cr>", desc = "Leet submit question" },
+			{ "<leader>lo", "<cmd>Leet open<cr>", desc = "Leet open question on browser" },
+		},
 	},
 	{ "ThePrimeagen/vim-be-good", cmd = "VimBeGood" },
 }
