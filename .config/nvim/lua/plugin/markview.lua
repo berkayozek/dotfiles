405caeef3019343484fca2309cfd0f2189136505
Berkay Ozek <berkayozek@hotmail.com>
2025-06-15 15:45:27 +0200

    refactor(nvim): update markview config and remove markdown autocmd

--- .config/nvim/lua/plugin/markview.lua
+++ .config/nvim/lua/plugin/markview.lua
@@ -1,7 +1,16 @@
 return {
 	"OXY2DEV/markview.nvim",
-    event = { 'BufReadPre', 'BufNewFile' },
-	preview = {
-		icon_provider = "mini",
-	},
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local presets = require("markview.presets")
+		require("markview").setup({
+			preview = {
+				icon_provider = "mini",
+			},
+			markdown = {
+				headings = presets.headings.arrowed,
+				horizontal_rules = presets.horizontal_rules.thick
+			},
+		})
+	end,
 }
