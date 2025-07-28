8d216f16b7bb7a12d44049c8f0bd926508a2ab32
Berkay Ozek <berkayozek@hotmail.com>
2025-07-28 17:50:51 +0200

    refactor: remap debug keybindings

--- .config/nvim/lua/plugin/debugging.lua
+++ .config/nvim/lua/plugin/debugging.lua
@@ -40,8 +40,8 @@ return {
 			vim.keymap.set("n", "<F5>", dap.continue, { desc = "Debug: Start/Continue" })
 			vim.keymap.set("n", "<F9>", dap.toggle_breakpoint, { desc = "Debug: Toggle Breakpoint" })
 			vim.keymap.set("n", "<F10>", dap.step_over, { desc = "Debug: Step Over" })
-			vim.keymap.set("n", "<F11>", dap.step_into, { desc = "Debug: Step Into" })
-			vim.keymap.set("n", "<F12>", dap.step_out, { desc = "Debug: Step Out" })
+			vim.keymap.set("n", "<F6>", dap.step_into, { desc = "Debug: Step Into" })
+			vim.keymap.set("n", "<F7>", dap.step_out, { desc = "Debug: Step Out" })
 
 			-- More detailed keymaps
 			vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, { desc = "Debug: Toggle Breakpoint" })
