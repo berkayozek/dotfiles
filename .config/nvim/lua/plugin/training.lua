7e5391194b9d57ef789d50407cee9fe4a74a9ca1
Berkay Ozek <berkayozek@hotmail.com>
2025-10-06 12:38:58 +0200

    feat(nvim): deattach lsp when open leetcode.nvim

--- .config/nvim/lua/plugin/training.lua
+++ .config/nvim/lua/plugin/training.lua
@@ -1,3 +1,27 @@
+local function is_leetcode_buffer(bufnr)
+  local buf_name = vim.api.nvim_buf_get_name(bufnr or 0)
+  return buf_name and string.find(buf_name, "/leetcode/", 1, true)
+end
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("DisableLspForLeetCode", { clear = true }),
+  callback = function(args)
+    -- We need the buffer number from the arguments
+    local bufnr = args.buf
+
+    -- Check if the path contains the leetcode directory
+    if is_leetcode_buffer(bufnr) then
+      -- CRITICAL: Defer the detach operation.
+      -- This waits until the initial LSP setup is complete, avoiding errors.
+      vim.schedule(function()
+        vim.lsp.buf_detach_client(bufnr, args.data.client_id)
+      end)
+      vim.diagnostic.enable(false)
+      vim.b.completion = false
+    end
+  end,
+})
+
 return {
 	{
 		"kawre/leetcode.nvim",
@@ -25,6 +49,7 @@ return {
 			{ "<leader>ls", "<cmd>Leet submit<cr>", desc = "Leet submit question" },
 			{ "<leader>lo", "<cmd>Leet open<cr>", desc = "Leet open question on browser" },
 			{ "<leader>lh", "<cmd>Leet hints<cr>", desc = "Leet open hints" },
+			{ "<leader>lf", "<cmd>Leet fold<cr>", desc = "Leet fold" },
 		},
 	},
 	{ "ThePrimeagen/vim-be-good", cmd = "VimBeGood" },
